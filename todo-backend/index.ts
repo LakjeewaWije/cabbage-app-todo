8070709d6db4a79b27df5f92da8c9bb41a49cf85
Lakjeewa WIjebandara <lakjeewa.wijebandara@gmail.com>
2021-06-21 22:44:24 +0530

    initiate create todo route - add bodyparser - add route to create todo

--- todo-backend/index.ts
+++ todo-backend/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+var bodyParser = require('body-parser')
 import { DatabaseMongoDB } from './providers/Database.provider.mongodb';
 // rest of the code remains same
 const app = express();
+
 const PORT = 8000;
 
 DatabaseMongoDB.init();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // routes
 var todoRoutes = require('./routes/todo.route');
--- todo-backend/routes/todo.route.ts
+++ todo-backend/routes/todo.route.ts
@@ -10,11 +10,23 @@ router.use(function timeLog(req: any, res: any, next: () => void) {
   next();
 });
 
-// define the home page route
+/**
+ * Get all todos
+ */
 router.get("/", async function (req: Request, res: Response) {
   const todos = await TodoController.getAllTodos();
   console.log(todos);
   res.send(todos);
 });
+
+/**
+ * Add new todo
+ */
+ router.post("/", async function (req: Request, res: Response) {
+     const data : TodoInterface  = req.body;
+    // const todos = await TodoController.getAllTodos();
+    console.log("creating this record",data);
+    res.send(data);
+  });
 
 module.exports = router;
